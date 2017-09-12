65f0a997ccff548d3c60edc12e0888f5e98bc518
Michael Graessle <mike-git@graessle.net>
2017-09-12 08:47:52 -0500

    Write cfg after changes

--- go/src/github.com/mohaine/beerboard/beerboard.go
+++ go/src/github.com/mohaine/beerboard/beerboard.go
@@ -27,7 +27,9 @@ func updateCfg(cfg *Configuration, configurationParam string) (e error) {
 		if e == nil {
 			cfg.Beers = newCfg.Beers
 			cfg.Taps = newCfg.Taps
+			WriteConfiguration(cfg)
 		}
+
 	}
 	return
 }
@@ -56,6 +58,7 @@ func updateCfgBeer(cfg *Configuration, beerParam string) (e error) {
 			if !found {
 				cfg.Beers = append(beers, newBeer)
 			}
+			WriteConfiguration(cfg)
 		}
 	}
 	return
@@ -80,6 +83,7 @@ func updateCfgTap(cfg *Configuration, tapParm string) (e error) {
 			if !found {
 				cfg.Taps = append(taps, newTap)
 			}
+			WriteConfiguration(cfg)
 		}
 	}
 	return
